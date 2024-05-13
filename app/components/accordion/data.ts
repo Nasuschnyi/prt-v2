0c571555025e64de2bb103fb7e99eccaa2f469b9
Nasuschnyi <91079054+Nasuschnyi@users.noreply.github.com>
2024-05-13 12:11:58 +0300

    add-info

--- app/components/accordion/data.ts
+++ app/components/accordion/data.ts
@@ -1,29 +1,52 @@
 const accordionData = [
-    {
-        id: 1,
-        title: 'As a Front-end Developer',
-        content: 'I possess a diverse skill set and expertise in HTML5 / CSS3 / SCSS / Sass / JavaScript / Git / BEM / React and Next.js. With my proficiency in these technologies, I create visually appealing and responsive web applications while adhering to industry standards.'
-    },
-    {
-        id: 2,
-        title: 'My knowledge',
-        content: 'of HTML5 and CSS3 enables me to build structured and visually captivating web pages, leveraging the latest semantic elements and CSS properties. I am well-versed in SCSS, Sass, allowing me to develop modular and reusable styles, facilitating efficient and streamlined development processes.'
-    },
-    {
-        id: 3,
-        title: 'Proficient in JavaScript',
-        content: 'I implement dynamic functionalities on the client-side, ensuring smooth interactivity and seamless integration with APIs. Git is an essential tool in my toolkit, empowering me to manage version control and collaborate effectively with other developers.'
-    },
-    {
-        id: 4,
-        title: 'I embrace the BEM',
-        content: '(Block-Element-Modifier) methodology, ensuring clean and maintainable code structure, which promotes scalability and reusability across projects. Furthermore, my experience in React and Next.js enables me to build robust and performant web applications, leveraging their component-based architecture, server-side rendering, and optimization techniques.'
-    },
-    {
-        id: 5,
-        title: 'Moreover',
-        content: 'I am well-versed in Search Engine Optimization (SEO) techniques, adhering to W3C standards, and ensuring cross-browser and cross-device compatibility. By following best practices and employing responsive design principles, I create web applications that are highly visible to search engines, deliver an excellent user experience, and seamlessly function across different browsers and devices.'
-    }
-]
+	{
+		id: 1,
+		title: 'As a Front-end Developer',
+		content:
+			'I possess a diverse skill set and expertise in HTML5 / CSS3 / SCSS / Sass / JavaScript / Git / BEM / React and Next.js, TypeScript. With my proficiency in these technologies, I create visually appealing and responsive web applications while adhering to industry standards.',
+	},
+	{
+		id: 2,
+		title: 'My knowledge',
+		content:
+			'of HTML5 and CSS3 enables me to build structured and visually captivating web pages, leveraging the latest semantic elements and CSS properties. I am well-versed in SCSS, Sass, allowing me to develop modular and reusable styles, facilitating efficient and streamlined development processes.',
+	},
+	{
+		id: 3,
+		title: 'Proficient in JavaScript',
+		content:
+			'I implement dynamic functionalities on the client-side, ensuring smooth interactivity and seamless integration with APIs. Git is an essential tool in my toolkit, empowering me to manage version control and collaborate effectively with other developers.',
+	},
+	{
+		id: 4,
+		title: 'I embrace the BEM',
+		content:
+			'(Block-Element-Modifier) methodology, ensuring clean and maintainable code structure, which promotes scalability and reusability across projects. Furthermore, my experience in React and Next.js enables me to build robust and performant web applications, leveraging their component-based architecture, server-side rendering, and optimization techniques.',
+	},
+	{
+		id: 5,
+		title: 'Moreover',
+		content:
+			'I am well-versed in Search Engine Optimization (SEO) techniques, adhering to W3C standards, and ensuring cross-browser and cross-device compatibility. By following best practices and employing responsive design principles, I create web applications that are highly visible to search engines, deliver an excellent user experience, and seamlessly function across different browsers and devices.',
+	},
+	{
+		id: 6,
+		title: 'Experience with Testing and Debugging',
+		content:
+			'I have experience with various testing and debugging tools, which helps me to ensure the quality of my code and deliver bug-free applications.',
+	},
+	{
+		id: 7,
+		title: 'Understanding of Web Performance',
+		content:
+			'I understand the importance of web performance and follow best practices to optimize load times and improve the user experience. This includes techniques such as code splitting, lazy loading, and using performance analysis tools.',
+	},
+	{
+		id: 8,
+		title: 'Collaboration and Communication',
+		content:
+			'I value collaboration and have experience working in diverse teams. I am comfortable using collaboration tools and platforms, and I believe in clear and effective communication to ensure everyone is on the same page.',
+	},
+];
 
 export default accordionData;
